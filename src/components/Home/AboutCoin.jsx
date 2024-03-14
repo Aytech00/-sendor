a94124353b1671d75331e27fa193d1367c7aebf4
Aytech <ayokunumitoluwani00@gmail.com>
2024-03-14 02:39:40 +0100

    init

--- src/components/Home/AboutCoin.jsx
+++ src/components/Home/AboutCoin.jsx
@@ -9,11 +9,7 @@ export default function AboutCoin() {
           <div className=" w-full mb-10 md:mb-0 md:w-[40%]  ">
             <img className=" " src={img2} alt="" />
           </div>
-          <div className="  w-full text-center md:text-left  md:w-[40%] text-[15px]  ">
-            <p className="leading-loose">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, aspernatur labore commodi, velit minima illo adipisci cumque veritatis ea laudantium temporibus iure facilis placeat aliquid numquam laboriosam ducimus magnam. Alias ut a sequi accusantium ullam libero ipsa nisi? Magnam officia eligendi natus molestiae, delectus magni quo sapiente reiciendis nostrum vero nemo suscipit ipsam possimus tempore optio minus ab mollitia ad!
-            </p>
-          </div>
+          
         </div>
       </div>
     </section>
